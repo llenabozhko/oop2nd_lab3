0f17350be5d34ae0d41b8ae545c60998a47ce343
Olena Bozhko <bozhko.olena1123@vu.cdu.edu.ua>
2025-05-15 22:05:34 +0300

    1 блок виконано. жартівливий додаток де одна кнопка втікає від іншої на рандомну відстань. були використані обробники подій кліка на кнопку та на них причеплені методи. також реалізовано щоб кнопка ні не натискалася табом

---
+++ block1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace block1
+{
+    public partial class MainWindow : Window
+    {
+        private Random _random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NoButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            double maxX = Math.Max(0, ActualWidth - NoButton.ActualWidth - 40);
+            double maxY = Math.Max(0, ActualHeight - NoButton.ActualHeight - 100);
+
+            double newX = _random.NextDouble() * maxX;
+            double newY = _random.NextDouble() * maxY;
+
+            Canvas.SetLeft(NoButton, newX);
+            Canvas.SetTop(NoButton, newY);
+        }
+
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("А ми і не сумнівалися ;)", "Відповідь", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void NoButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Який ви настирний!", "Відповідь", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.FocusedElement == YesButton)
+            {
+                NoButton.Focus();
+            }
+        }
+    }
+}
